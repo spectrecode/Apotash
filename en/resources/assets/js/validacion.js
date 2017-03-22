ec6d4bfd6d8533fb358ed193aca90a574e55f773
spectrecode <mchristy.ccb@gmail.com>
2017-03-22 17:20:33 -0500

    añadiendo version en ingles

---
+++ en/resources/assets/js/validacion.js
@@ -0,0 +1,31 @@
+// var valForm = function(){
+// 	$("#btn-form").click(valDatos);
+// }
+// $(document).ready(valForm);
+
+// var valDatos = function(){
+// 	 var regexEmail = /^[a-zA-Z0-9_\.\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$/;
+// 	 var inputNombre = $("#name").val();
+// 	 var inputAdress = $("#adress").val();
+// 	 var inputMail = $("#mail").val();
+// 	 var inputTelf = $("#telf").val();
+// 	 var inputComment = $("#comment").val();
+	
+// 	if (inputNombre == "") {
+// 		$(".msg-error").focus().append("Escriba su nombre");
+// 		return false
+// 	}
+// 	else if (inputNombre.length < 3 ) {
+// 		$(".msg-error").focus().after("Escriba un nombre válido");
+// 		// alert("hola");
+// 	}
+// 	// if (inputAdress == "") {
+// 	// 	$(".msgA-error").append("Escriba su dirección");
+// 	// }
+// 	// if (inputAdress.length < 3 || inputAdress.length > 30) {
+// 	// 	$(".msgA-error").append("Escriba una dirección válida");
+// 	// }
+// 	// if (inputTelf == "" && inputTelf != isNaN) {
+// 	// 	$(".msgT-error").append("Escriba su número de teléfono");
+// 	// }
+// }
